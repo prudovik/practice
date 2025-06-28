c4cbc2aa4244896c2e354725a04477639fc78450
prudovik <yaktoblin@gmail.com>
2025-06-28 20:53:37 +0600

    feat: add extension func realization

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace task01;
+
+using System;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        input = input.ToLower();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (Char.IsPunctuation(input, i) || Char.IsWhiteSpace(input, i))
+            {
+                input = input.Remove(i, 1);
+                i--;
+            }
+        }
+        if (input == "") return false;
+        for (int i = 0; i < input.Length / 2; i++)
+        {
+            if (input[i] != input[input.Length - (1 + i)]) return false;
+        }
+        return true;
+    }
+}
