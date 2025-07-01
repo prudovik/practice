1566b03cda754964b63ea3d4c905bd7a24d4f610
prudovik <yaktoblin@gmail.com>
2025-07-01 19:24:19 +0600

    feat: add classanalyzer realization

---
+++ task05/ClassAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿namespace task05;
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+    => _type.GetMethods().Select(member => member.Name);
+
+    public IEnumerable<string?> GetMethodParams(string methodname)
+    {
+        var method = _type.GetMethod(methodname);
+        return method.GetParameters().Select(parameter => parameter.Name);
+    }
+
+    public IEnumerable<string> GetAllFields()
+    => _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static).Select(member => member.Name);
+
+    public IEnumerable<string> GetProperties()
+    => _type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static).Select(member => member.Name);
+
+    public bool HasAttribute<T>() where T : Attribute
+    => _type.GetCustomAttributes(typeof(T), false).Length > 0;
+}
