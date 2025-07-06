04532d71426c01a58e51e46e6b927130743ebd7f
prudovik <yaktoblin@gmail.com>
2025-07-06 06:35:00 +0600

    feat: add attributes and classes realization

---
+++ task07/SampleClass.cs
@@ -0,0 +1,11 @@
+﻿namespace task07;
+
+[DisplayNameAttribute("Пример класса"), VersionAttribute(1, 0)]
+public class SampleClass
+{
+    [DisplayNameAttribute("Тестовый метод")]
+    public void TestMethod() { }
+
+    [DisplayNameAttribute("Числовое свойство")]
+    public int Number { get; set; }
+}
