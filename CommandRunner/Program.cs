b390f24bb75b9b790e6cbbd01f3e3e37a5af17be
prudovik <yaktoblin@gmail.com>
2025-07-07 22:19:09 +0600

    feat: add console app

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CommandRunner;
+
+using System.Reflection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Assembly asm = Assembly.LoadFrom("FileSystemCommands.dll");
+        ar testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+        var command = new DirectorySizeCommand(testDir);
+        command.Execute();
+        Directory.Delete(testDir, true);
+
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+        File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+        var command = new FindFilesCommand(testDir, "*.txt");
+        command.Execute();
+        Directory.Delete(testDir, true);
+    }
+}
+
