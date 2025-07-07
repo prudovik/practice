623cd1ef237a126a5c0f9731c5d865643d322e49
prudovik <yaktoblin@gmail.com>
2025-07-07 22:30:16 +0600

    fix: fix programs work

--- CommandRunner/Program.cs
+++ CommandRunner/Program.cs
@@ -1,27 +1,29 @@
 ﻿namespace CommandRunner;
 
 using System.Reflection;
+using System.IO;
+using CommandLib;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Assembly asm = Assembly.LoadFrom("FileSystemCommands.dll");
-        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
-        Directory.CreateDirectory(testDir);
-        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
-        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
-        var command = new DirectorySizeCommand(testDir);
-        command.Execute();
-        Directory.Delete(testDir, true);
+        var testDir1 = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir1);
+        File.WriteAllText(Path.Combine(testDir1, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir1, "test2.txt"), "World");
+        var command1 = new DirectorySizeCommand(testDir1);
+        command1.Execute();
+        Directory.Delete(testDir1, true);
 
-        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
-        Directory.CreateDirectory(testDir);
-        File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
-        File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
-        var command = new FindFilesCommand(testDir, "*.txt");
-        command.Execute();
-        Directory.Delete(testDir, true);
+        var testDir2 = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir2);
+        File.WriteAllText(Path.Combine(testDir2, "file1.txt"), "Text");
+        File.WriteAllText(Path.Combine(testDir2, "file2.log"), "Log");
+        var command2 = new FindFilesCommand(testDir2, "*.txt");
+        command2.Execute();
+        Directory.Delete(testDir2, true);
     }
 }
 
