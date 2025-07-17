0f8dcbe0f03ebde19caa27f8db348ed864569ade
prudovik <yaktoblin@gmail.com>
2025-07-17 15:47:33 +0600

    feat: add task13 realization

---
+++ task13/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace task13;
+
+public class Subject
+{
+  public string Name {get; set; }
+  public int Grade {get; set; }
+}
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public List<Subject> Grades { get; set; }
+}
