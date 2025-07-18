6a25d61088dcfd267f1e1ef02a24be804030975a
prudovik <yaktoblin@gmail.com>
2025-07-19 03:21:58 +0600

    feat: add serverthread realisation

---
+++ task17/HardStop.cs
@@ -0,0 +1,17 @@
+namespace task17;
+
+using System.Threading;
+using System.Collections.Concurrent;
+
+public class HardStopper : ICommand
+{
+    private readonly ServerThread server;
+    public HardStopper(ServerThread s)
+    {
+        server = s;
+    }
+    public void Execute()
+    {
+        server.HardStop();
+    }
+}
---
+++ task17/ICommand.cs
@@ -0,0 +1,6 @@
+namespace task17;
+
+public interface ICommand
+{
+    void Execute();
+}
